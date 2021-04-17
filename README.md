d32867dbacbf3aeb6419794e4169af8c708a833f
ionut270 <oanceaionut15@gmail.com>
2021-04-17 23:02:32 +0300

    Added mongoDB connection

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # azure-cc
+
+## Services
+Active directory AUTH
+Resource Group Stuff
+Compute
+Monitoring Stuff 
+MongoDb
--- index.js
+++ index.js
@@ -11,7 +11,24 @@ app.listen(port,(err)=>{
     else console.log(`App running on port ${port}`);
 })
 
+// Define an object containing the status of the app
+var status = {
+    app : "OK",
+    mongo : null
+}
+
 // App router
 app.get('/',(req,res)=>{
-    res.status(200).send("Hello World");
+    res.status(200).send(status);
 })
+
+var mongoClient = require("mongodb").MongoClient;
+mongoClient.connect(process.env.MONGO_CONN_STRING,{ useUnifiedTopology: true },  function (err, client) {
+    if(err){
+        console.log(err);
+        status.mongo = err;
+    } else {
+        status.mongo = "Connected to mongodb !";
+    }
+  client.close();
+});
